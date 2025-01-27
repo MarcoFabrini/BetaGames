15cc2214bdd5e35008292c701045de3596e0ae85
Marco Fabrini <fabrinimarco@protonmail.com>
2025-01-27 17:41:53 +0100

    first commit

---
+++ betaGames/src/main/java/com/betaGames/BetaGamesApplication.java
@@ -0,0 +1,13 @@
+package com.betaGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetaGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetaGamesApplication.class, args);
+	}
+
+}
---
+++ betaGames/src/test/java/com/betaGames/BetaGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betaGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetaGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

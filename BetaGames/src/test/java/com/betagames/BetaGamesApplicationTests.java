b097f7aa9aeefd23411b9e26b4de4437375d6d51
Marco Fabrini <fabrinimarco@protonmail.com>
2025-01-27 20:04:06 +0100

    First commit
    Created project folder

---
+++ BetaGames/src/test/java/com/betagames/BetaGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betagames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetaGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

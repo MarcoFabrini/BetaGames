16fc99b223f43640a293f9a89ba8a1ebd4badd0c
Marco Fabrini <fabrinimarco@protonmail.com>
2025-01-30 19:45:59 +0100

    work in my branch
    Repository (Editors, Reviews, Users), Request (Editors, Reviews, Users),
    Response (base, list, object),
    Interfaces (Editors),
    Implementation (Editors)

---
+++ BetaGames/src/main/java/com/betagames/repository/IReviewsRepository.java
@@ -0,0 +1,13 @@
+package com.betagames.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.betagames.model.Reviews;
+
+/**
+ *
+ * @author Fabrini Marco
+ */
+public interface IReviewsRepository extends JpaRepository<Reviews, Integer>{
+
+}// interface

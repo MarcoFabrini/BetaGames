cb6d176ef5b71a238fb0a212e332dfe9cd69ed7b
Cristhian Danilo Guerrero Bautista <guerrerocristhian.coder@gmail.com>
2025-01-31 17:41:18 +0100

    Modify Authors

---
+++ BetaGames/src/main/java/com/betagames/service/implementation/AuthorsImplementation.java
@@ -0,0 +1,59 @@
+package com.betagames.service.implementation;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.betagames.dto.AuthorsDTO;
+import com.betagames.model.Authors;
+import com.betagames.repository.IAuthorsRepository;
+import com.betagames.request.AuthorsRequest;
+import com.betagames.service.interfaces.IAuthorsService;
+
+/**
+ *
+ * @author Cristhian Guerrero
+ */
+@Service
+public class AuthorsImplementation implements IAuthorsService{
+
+    @Autowired
+    IAuthorsRepository authorsRepository;
+
+    @Override
+    public void create(AuthorsRequest req) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'create'");
+    }
+
+    @Override
+    public void update(AuthorsRequest req) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void delete(AuthorsRequest req) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    @Override
+    public List<AuthorsDTO> searchByTyping(Integer id, String name, String lastname, String country, String biography,
+            Integer gameId) throws Exception {
+
+            List<Authors> listAuthors = authorsRepository.searchByFilter(id, name, lastname, country, biography, gameId);
+
+            // return listAuthors.stream()
+            //         .map(s -> new AuthorsDTO(
+            //             s.getId(), 
+            //             s.getBiography(), 
+            //             s.getCountry(), 
+            //             s.getLastname(), 
+            //             s.getName(), 
+            //             s.getListGames()))
+     }
+
+
+}// class
--- BetaGames/src/main/java/com/betagames/utility/Utilities.java
+++ BetaGames/src/main/java/com/betagames/utility/Utilities.java
@@ -1,5 +1,29 @@
 package com.betagames.utility;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+import com.betagames.dto.GamesDTO;
+import com.betagames.model.Games;
+
 public class Utilities {
     
+    // 	public static List<GamesDTO> buildAbbonamentoDTO(List<Games> games) {
+	// 	return games.stream()
+	// 			.map(a -> new GamesDTO(
+    //                 a.getId(),
+    //                 a.getName(),
+    //                 a.getDate(),
+    //                 a.getMinGameTime(),
+    //                 a.getMaxGameTime(),
+    //                 a.getMinPlayerNumber(),
+    //                 a.getMaxPlayerNumber(),
+    //                 a.getMinAge(),
+    //                 a.getDescription(),
+    //                 a.getStockQuantity(),
+    //                 a.getPrice(),
+    //                 a.getListAuthors()))
+	// 			.collect(Collectors.toList());
+	// }
 }

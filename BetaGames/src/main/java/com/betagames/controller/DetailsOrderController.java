e5ba75ebfb252db76908d0c7fbd42102f717f150
Simone <118662447+Chebaz@users.noreply.github.com>
2025-02-06 14:23:13 +0100

    add detailsOrder

---
+++ BetaGames/src/main/java/com/betagames/controller/DetailsOrderController.java
@@ -0,0 +1,85 @@
+package com.betagames.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.betagames.dto.DetailsOrderDTO;
+import com.betagames.request.DetailsOrderRequest;
+import com.betagames.response.ResponseBase;
+import com.betagames.response.ResponseList;
+import com.betagames.service.interfaces.IDetailsOrderService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@RestController
+@RequestMapping("/rest/details")
+public class DetailsOrderController {
+    @Autowired
+    IDetailsOrderService detOrderS;
+
+    @GetMapping("/readDetailsOrder")
+    public ResponseList<DetailsOrderDTO> listDetailsOrder(@RequestParam ("id")Integer id) {
+        ResponseList<DetailsOrderDTO> responseList = new ResponseList<DetailsOrderDTO>();
+        responseList.setRc(true);
+        System.out.println("Richiesta: " +id);
+        try {
+            responseList.setData(detOrderS.searchByOrder(id));
+        } catch (Exception e) {
+            responseList.setRc(false);
+            responseList.setMsg(e.getMessage());
+        }
+        return responseList;
+    }
+    
+
+    @PostMapping("/createDetailsOrder")
+    public ResponseBase create(@RequestBody(required = true) DetailsOrderRequest req) {
+        ResponseBase response = new ResponseBase();
+        response.setRc(true);
+
+        try {
+            detOrderS.create(req);
+        } catch (Exception e) {
+            response.setRc(false);
+            response.setMsg(e.getMessage());
+        }
+
+        return response;
+    }
+
+    @PostMapping("/updateDetailsOrder")
+    public ResponseBase update(@RequestBody(required = true) DetailsOrderRequest req) {
+        ResponseBase response = new ResponseBase();
+        response.setRc(true);
+
+        try {
+            detOrderS.update(req);
+        } catch (Exception e) {
+            response.setRc(false);
+            response.setMsg(e.getMessage());
+        }
+        
+        return response;
+    }
+    
+    @PostMapping("/deleteDetailsOrder")
+    public ResponseBase delete(@RequestBody(required = true) DetailsOrderRequest req){
+        ResponseBase response = new ResponseBase();
+        response.setRc(true);
+
+        try {
+            detOrderS.delete(req);
+        } catch (Exception e) {
+            response.setRc(false);
+            response.setMsg(e.getMessage());
+        }
+        return response;
+    }
+    
+}

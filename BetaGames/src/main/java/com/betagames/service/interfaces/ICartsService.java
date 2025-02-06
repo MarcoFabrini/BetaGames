6e4dcf088861a840de16e13298e47805f7eb55fc
Marco Fabrini <fabrinimarco@protonmail.com>
2025-02-06 15:12:12 +0100

    deleted log

--- BetaGames/src/main/java/com/betagames/service/interfaces/ICartsService.java
+++ BetaGames/src/main/java/com/betagames/service/interfaces/ICartsService.java
@@ -1,5 +1,4 @@
 package com.betagames.service.interfaces;
-import java.util.List;
 
 import com.betagames.dto.CartsDTO;
 import com.betagames.request.CartsRequest;
@@ -7,10 +6,12 @@ import com.betagames.request.CartsRequest;
 public interface ICartsService {
 
     void create(CartsRequest req) throws Exception;
+
     void update(CartsRequest req) throws Exception;
+
     void remove(CartsRequest req) throws Exception;
 
-    //visulizzo il carrello del utente con annesso il details_cart
+    // visulizzo il carrello del utente con annesso il details_cart
     CartsDTO listCartsByUser(Integer id) throws Exception;
 
 }

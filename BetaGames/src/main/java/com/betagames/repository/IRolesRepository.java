65a6caac996ddaa6d8ee8a719c8e0a810e115e44
Matteo-Nardin <123728836+Matteo-Nardin@users.noreply.github.com>
2025-02-11 10:23:46 +0100

    junit utilities

--- BetaGames/src/main/java/com/betagames/repository/IRolesRepository.java
+++ BetaGames/src/main/java/com/betagames/repository/IRolesRepository.java
@@ -15,5 +15,7 @@ import com.betagames.model.Roles;
 public interface IRolesRepository extends JpaRepository<Roles,Integer>{
 
     Optional<Roles> findByName(String name);
+
+    Optional<Roles> findByNameIgnoreCase(String name);
     
 }
